4d4418e4985b4508de650fea78c2f36862893430
Imran Price <reindurr7@gmail.com>
2020-05-21 00:34:19 +1200

    fixed typos

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,30 +1,44 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-export default function Person({ownersList}){
-    const router = useRouter();
-    const [owners, setOwners] = useState(ownersList);
-    useEffect(() => {
+export default function Person({ ownersList }) {
+  const router = useRouter();
+
+ const [owners, setOwners] = useState(ownersList);
+  useEffect(() => {
     async function loadData() {
-        const response = await fetch('http://localhost:4000/vehicles?ownerName=' + router.query.person + '&vehicle='+router.query.vehicle);
-        const ownersList = await response.json();
+      const response = await fetch('http://localhost:4000/vehicles?ownerName=' +
+      router.query.person +
+      '&vehicle=' +
+      router.query.vehicle);
+      const ownersList = await response.json();
       setOwners(ownersList);
     }
-    if(ownersList.length == 0){
-       loadData();
+
+    if(ownersList.length == 0) {
+        loadData();
     }
-    
   }, []);
 
-return <pre>{ownersList[0]?.details}</pre>
+  if(!owners[0]) { 
+      return <div>loading...</div>
+  }
+
+  return <pre>{owners[0]?.details}</pre>;
 }
 
-Person.getInitialProps = async (context) => {
-    if(!context.req){
-        return {ownersList: []};
+Person.getInitialProps = async ctx => {
+    if(!ctx.req) {
+        return { ownersList: [] };
     }
-    const {query} = context
-    const response = await fetch('http://localhost:4000/vehicles?ownerName=' + query.person + '&vehicle='+query.vehicle);
-    const ownersList = await response.json();
-    return {ownersList: ownersList}
-  }
+
+  const { query } = ctx;
+  const response = await fetch(
+    'http://localhost:4000/vehicles?ownerName=' +
+      query.person +
+      '&vehicle=' +
+      query.vehicle
+  );
+  const ownersList = await response.json();
+  return { ownersList: ownersList };
+};
